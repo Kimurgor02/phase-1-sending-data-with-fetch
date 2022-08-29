d08ea9f63bf2fdd9f4a6b5503b8be4ddb2ab728c
Allan <allan.kimurgor@student.moringaschool.com>
2022-08-29 07:43:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+const body = document.querySelector("body");
+function submitData (userName, userEmail){
+	
+	
+	const userData = {
+		name: userName,
+		email: userEmail
+	};
+	
+	const handleUserData = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		
+		body: JSON.stringify(userData),
+	};
+
+	let action = fetch("http://localhost:3000/users", handleUserData)
+	.then(resp => resp.json())
+	.then(json => {
+		body.append(json.id);
+	})
+	.catch( error => {
+		alert("A bug detected!!");
+		body.append(error);
+    });
+    return action;
+  }
